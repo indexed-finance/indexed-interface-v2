2bae8bdbc13bc87640cc1533d79e6799cdef39f7
d1ll0n <dillonkellar@protonmail.com>
2021-03-11 14:41:58 -0600

    add LineSeriesChart molecule

---
+++ src/components/molecules/LineSeriesChart.tsx
@@ -0,0 +1,107 @@
+import { IChartApi, ISeriesApi, createChart } from "lightweight-charts";
+import { colors } from "theme";
+import { selectors } from "features";
+import { useSelector } from "react-redux";
+import React, { useEffect, useRef, useState } from "react";
+
+export interface SeriesDataItem {
+  time: number;
+  value: number;
+}
+
+export interface Props {
+  data: SeriesDataItem[];
+  expanded?: boolean;
+}
+
+export default function LineSeriesChart({ data, expanded = false }: Props) {
+  const theme = useSelector(selectors.selectTheme);
+  const cardRef = useRef<null | HTMLDivElement>(null);
+  const [chart, setChart] = useState<IChartApi | null>(null);
+  const [series, setSeries] = useState<ISeriesApi<"Line"> | null>(null);
+
+  useEffect(() => {
+    if (cardRef.current && !series) {
+      const size = expanded
+        ? { width: 1200, height: 500 }
+        : { width: 400, height: 300 };
+      
+      const chart_ = createChart(cardRef.current, size);
+      setChart(chart_);
+      const options = CHART_MODES[theme];
+      const lineSeries = chart_.addLineSeries();
+      chart_.applyOptions(options);
+      setSeries(lineSeries);
+      setTimeout(() => {
+        if (cardRef.current) {
+          chart_.resize(
+            cardRef.current.clientWidth,
+            cardRef.current.clientHeight
+          );
+        }
+      }, 250);
+    }
+  }, [expanded, theme]);
+
+  useEffect(() => {
+    if (cardRef.current && series) {
+      series.setData(data as any);
+    }
+  }, [data, series]);
+
+  useEffect(() => {
+    if (cardRef.current && chart) {
+      const [width, height] = expanded ? [1200, 500] : [400, 300];
+      const options = CHART_MODES[theme];
+      chart.resize(width, height);
+      chart.applyOptions(options);
+      setTimeout(() => {
+        if (cardRef.current) {
+          chart.resize(
+            cardRef.current.clientWidth,
+            cardRef.current.clientHeight
+          );
+        }
+      }, 250);
+    }
+  }, [expanded, theme, chart])
+
+  return <div ref={cardRef} />
+}
+
+const COMMON_LAYOUT_OPTIONS = {
+  fontFamily: "sans-serif",
+  fontSize: 16,
+};
+const CHART_MODES = {
+  dark: {
+    layout: {
+      ...COMMON_LAYOUT_OPTIONS,
+      backgroundColor: colors.black400,
+      textColor: colors.purple200,
+    },
+    grid: {
+      vertLines: {
+        color: colors.purple100,
+      },
+      horzLines: {
+        color: colors.purple100,
+      },
+    },
+  },
+  light: {
+    layout: {
+      ...COMMON_LAYOUT_OPTIONS,
+      backgroundColor: colors.white300,
+      textColor: colors.black200,
+    },
+    grid: {
+      vertLines: {
+        color: colors.purple300,
+      },
+      horzLines: {
+        color: colors.purple300,
+      },
+    },
+  },
+};
