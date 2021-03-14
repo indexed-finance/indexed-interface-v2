2de2d18decd48636755e5a7fe2b6a214baddb972
d1ll0n <dillonkellar@protonmail.com>
2021-03-13 20:05:13 -0600

    broken: Use TwoTokenExchangeForm for swap & trade -- swap incomplete

--- src/app/interactions/SwapInteraction.tsx
+++ src/app/interactions/SwapInteraction.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { AppState, FormattedIndexPool, selectors, signer } from "features";
 import { Flipper, Token } from "components/atoms";
-import { Form, Space } from "antd";
+import { Button, Form, Space } from "antd";
 import { TokenExchangeRate, TokenSelector } from "components";
 import { actions } from "features";
 import { convert } from "helpers";
@@ -10,219 +10,77 @@ import { useCallback, useMemo, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   useHistoryChangeCallback,
-  useTokenApproval,
+  // useTokenApproval,
   useTokenRandomizer,
 } from "./common";
+import TwoTokenExchangeForm, { TwoTokenExchangeFormValues } from "components/organisms/TwoTokenExchangeForm";
+import { ApprovalStatus } from "features/user/slice";
 
 interface Props {
-  pool: null | FormattedIndexPool;
+  pool: FormattedIndexPool;
 }
 
-type SwapValues = typeof INITIAL_STATE;
-
-const FIELD_OPPOSITES = {
-  from: "to",
-  to: "from",
-};
-const INITIAL_STATE = {
-  from: {
-    token: "",
-    amount: 0,
-  },
-  to: {
-    token: "",
-    amount: 0,
-  },
-};
-const SLIPPAGE_RATE = 0.01;
-
-const { Item } = Form;
 const ZERO = convert.toBigNumber("0");
+const SLIPPAGE_RATE = 0.01;
 
 export default function SwapInteraction({ pool }: Props) {
   const dispatch = useDispatch();
-  const [form] = Form.useForm<SwapValues>();
   const fullPool = useSelector((state: AppState) =>
-    selectors.selectPool(state, pool?.id ?? "")
+    selectors.selectPool(state, pool.id)
   );
-  const previousFormValues = useRef<SwapValues>(INITIAL_STATE);
-  const lastTouchedField = useRef<"input" | "output">("input");
+  const swapFee = useSelector((state: AppState) => selectors.selectSwapFee(state, pool.id));
   const tokenLookup = useSelector(selectors.selectTokenLookupBySymbol);
-  const swapFee = useSelector((state: AppState) =>
-    selectors.selectSwapFee(state, pool?.id ?? "")
-  );
-  const [price, setPrice] = useState(ZERO);
   const [maxPrice, setMaxPrice] = useState(ZERO);
-  const [formattedSwapFee, setFormattedSwapFee] = useState("");
-  const [renderCount, setRenderCount] = useState(0);
-  const baseline = previousFormValues.current.from.token;
-  const comparison = previousFormValues.current.to.token;
-  const previousFrom = useMemo(() => {
-    if (renderCount > -1) {
-      return previousFormValues.current.from;
+  const [specifiedSide, setSpecifiedSide] = useState<"input" | "output">("input");
+
+  const handleChange = useCallback(({ from, to, lastTouchedField }: TwoTokenExchangeFormValues) => {
+    if (!fullPool || !swapFee) return;
+    const inputToken = fullPool.tokens.entities[
+      tokenLookup[from.token.toLowerCase()].id.toLowerCase()
+    ];
+    const outputToken = fullPool.tokens.entities[
+      tokenLookup[to.token.toLowerCase()].id.toLowerCase()
+    ];
+    if (!inputToken || !outputToken) return;
+    if (lastTouchedField === "INPUT") {
+      const {
+        outputAmount,
+        spotPriceAfter,
+        isGoodResult
+      } = helpers.calculateOutputFromInput(
+        inputToken,
+        outputToken,
+        from.amount.toString(),
+        swapFee
+      );
+      if (isGoodResult) {
+        to.amount = outputAmount;
+        setMaxPrice(
+          helpers.upwardSlippage(spotPriceAfter, SLIPPAGE_RATE)
+        );
+        setSpecifiedSide("input");
+      }
+    } else {
+      const {
+        inputAmount,
+        spotPriceAfter,
+        isGoodResult,
+      } = helpers.calculateInputFromOutput(
+        inputToken,
+        outputToken,
+        to.amount.toString(),
+        swapFee
+      );
+      if (isGoodResult) {
+        setMaxPrice(
+          helpers.upwardSlippage(spotPriceAfter, SLIPPAGE_RATE)
+        );
+        from.amount = inputAmount;
+        setSpecifiedSide("output");
+      }
     }
-  }, [renderCount]);
-  const previousTo = useMemo(() => {
-    if (renderCount > -1) {
-      return previousFormValues.current.to;
-    }
-  }, [renderCount]);
-  const handleFlip = () => {
-    const { from, to } = previousFormValues.current;
-    const flippedValue = {
-      from: to,
-      to: from,
-    };
+  }, [setMaxPrice, fullPool, swapFee, tokenLookup]);
 
-    form.setFieldsValue(flippedValue);
-    previousFormValues.current = flippedValue;
-    triggerUpdate();
-  };
-  const getFormattedSwapFee = useCallback(
-    (outputAmount: number): string =>
-      convert
-        .toBigNumber(outputAmount.toString(10))
-        .times(
-          convert.toBigNumber(
-            (parseFloat(pool?.swapFee || "0") / 100).toString()
-          )
-        )
-        .toString(10),
-    [pool]
-  );
-  const triggerUpdate = () => setRenderCount((prev) => prev + 1);
-
-  /**
-   *
-   */
-  const checkForDuplicates = useCallback(
-    (changedValues: SwapValues) => {
-      for (const [changedField, changedValue] of Object.entries(
-        changedValues
-      )) {
-        const swapKey = changedField as keyof SwapValues;
-        const otherSwapKey = FIELD_OPPOSITES[swapKey] as keyof SwapValues;
-
-        if (otherSwapKey) {
-          const { [otherSwapKey]: otherFieldValue } = form.getFieldsValue();
-
-          if (changedValue.token === otherFieldValue.token) {
-            const {
-              [swapKey]: { token },
-            } = previousFormValues.current;
-
-            form.setFieldsValue({
-              [otherSwapKey]: {
-                token,
-              },
-            });
-          }
-        }
-      }
-    },
-    [form]
-  );
-  /**
-   *
-   */
-  const calculateOutputFromInput = useCallback(
-    async (changedValues: SwapValues) => {
-      if (fullPool && swapFee) {
-        const { amount: inputAmount } = changedValues.from;
-        const { token: inputToken } = previousFormValues.current.from;
-        const { token: outputToken } = previousFormValues.current.to;
-
-        if (inputToken && outputToken) {
-          const inputEntry = tokenLookup[inputToken.toLowerCase()];
-          const outputEntry = tokenLookup[inputToken.toLowerCase()];
-
-          if (inputEntry && outputEntry) {
-            const inputData =
-              fullPool.tokens.entities[
-                tokenLookup[inputToken.toLowerCase()].id.toLowerCase()
-              ];
-            const outputData =
-              fullPool.tokens.entities[
-                tokenLookup[outputToken.toLowerCase()].id.toLowerCase()
-              ];
-
-            if (inputData && outputData) {
-              const {
-                outputAmount,
-                price,
-                spotPriceAfter,
-                isGoodResult,
-              } = await helpers.calculateOutputFromInput(
-                inputData,
-                outputData,
-                inputAmount.toString(),
-                swapFee
-              );
-
-              if (isGoodResult) {
-                setPrice(price);
-                setMaxPrice(
-                  helpers.upwardSlippage(spotPriceAfter, SLIPPAGE_RATE)
-                );
-                setFormattedSwapFee(getFormattedSwapFee(outputAmount));
-
-                form.setFieldsValue({ to: { amount: outputAmount } });
-              }
-            }
-          }
-        }
-      }
-    },
-    [form, fullPool, tokenLookup, swapFee, getFormattedSwapFee]
-  );
-  /**
-   *
-   */
-  const calculateInputFromOutput = useCallback(
-    async (changedValues: SwapValues) => {
-      if (fullPool && swapFee) {
-        const { amount: outputAmount } = changedValues.to;
-        const { token: outputToken } = previousFormValues.current.to;
-        const { token: inputToken } = previousFormValues.current.from;
-        const inputEntry = tokenLookup[inputToken.toLowerCase()];
-        const outputEntry = tokenLookup[inputToken.toLowerCase()];
-
-        if (inputEntry && outputEntry) {
-          const inputData =
-            fullPool.tokens.entities[
-              tokenLookup[inputToken.toLowerCase()].id.toLowerCase()
-            ];
-          const outputData =
-            fullPool.tokens.entities[
-              tokenLookup[outputToken.toLowerCase()].id.toLowerCase()
-            ];
-
-          if (inputData && outputData) {
-            const {
-              inputAmount,
-              spotPriceAfter,
-              isGoodResult,
-            } = await helpers.calculateInputFromOutput(
-              inputData,
-              outputData,
-              outputAmount.toString(),
-              swapFee
-            );
-
-            if (isGoodResult) {
-              setMaxPrice(spotPriceAfter.times(SLIPPAGE_RATE));
-
-              form.setFieldsValue({ from: { amount: inputAmount } });
-            }
-          }
-        }
-      }
-    },
-    [form, fullPool, tokenLookup, swapFee]
-  );
-  /**
-   *
-   */
   const handleSubmit = useCallback(
     async (values: SwapValues) => {
       if (pool && signer) {
@@ -232,7 +90,7 @@ export default function SwapInteraction({ pool }: Props) {
         dispatch(
           actions.swap(
             pool.id,
-            lastTouchedField.current,
+            spec,
             inputAmount.toString(),
             inputToken.toLowerCase(),
             outputAmount.toString(),
@@ -244,112 +102,27 @@ export default function SwapInteraction({ pool }: Props) {
     },
     [dispatch, pool, maxPrice]
   );
-  /**
-   *
-   * @param _ -
-   * @param value -
-   */
-  const checkAmount = (_: any, value: { amount: number }) => {
-    return value.amount > 0
-      ? Promise.resolve()
-      : Promise.reject("Amount must be greater than zero.");
-  };
-  const handleSendTransaction = useCallback((values) => handleSubmit(values), [
-    handleSubmit,
-  ]);
-
-  useTokenRandomizer({
-    pool,
-    from: baseline,
-    to: comparison,
-    changeFrom: (newFrom: string) =>
-      form.setFieldsValue({
-        from: {
-          token: newFrom,
-        },
-      }),
-    changeTo: (newTo: string) =>
-      form.setFieldsValue({
-        to: {
-          token: newTo,
-        },
-      }),
-    callback: () => {
-      triggerUpdate();
-      previousFormValues.current = form.getFieldsValue();
-    },
+  
+  const { status, approve } = useTokenApproval({
+    spender: pool.id,
+    token,
+    amount
   });
 
-  useTokenApproval({
-    pool,
-    from: previousFrom!,
-    to: previousTo!,
-    onSendTransaction: handleSendTransaction,
-  });
-
-  useHistoryChangeCallback(() => form.resetFields());
-
-  return (
-    <Form
-      form={form}
-      name="swap"
-      size="large"
-      labelAlign="left"
-      layout="vertical"
-      initialValues={INITIAL_STATE}
-      onValuesChange={(changedValues) => {
-        checkForDuplicates(changedValues);
-
-        // Update previous values for future comparisons.
-        previousFormValues.current = form.getFieldsValue();
-
-        if (changedValues.from) {
-          lastTouchedField.current = "input";
-          calculateOutputFromInput(changedValues);
-        } else if (changedValues.to) {
-          lastTouchedField.current = "output";
-          calculateInputFromOutput(changedValues);
-        }
-
-        triggerUpdate();
-      }}
-      onFinish={(values: any) => handleSubmit(values)}
-      onFinishFailed={(error) =>
-        console.log("Submission failed. Error was: ", error)
-      }
-    >
-      <Space align="center" className="spaced-between">
-        {baseline && comparison && (
-          <div style={{ position: "relative", top: 0, right: 0 }}>
-            <Token name="Baseline" image={baseline} />
-            <AiOutlineArrowRight
-              style={{
-                position: "absolute",
-                fontSize: "32px",
-              }}
-            />
-            <Token name="Comparison" image={comparison} />
-          </div>
-        )}
-      </Space>
-      <Item name="from" rules={[{ validator: checkAmount }]}>
-        {pool && <TokenSelector label="From" assets={pool.assets} />}
-      </Item>
-      <Flipper onFlip={handleFlip} />
-      <Item name="to" rules={[{ validator: checkAmount }]}>
-        {pool && <TokenSelector label="To" assets={pool.assets} />}
-      </Item>
-      {previousFormValues.current.from.token &&
-        previousFormValues.current.to.token && (
-          <div>
-            <TokenExchangeRate
-              baseline={baseline}
-              comparison={comparison}
-              fee={formattedSwapFee}
-              rate={price.toString()}
-            />
-          </div>
-        )}
-    </Form>
-  );
+  return <TwoTokenExchangeForm
+    title={"Trade"}
+    assetOptions={assets as any}
+    onChange={handleChange}
+    defaultInput="DAI"
+    defaultOutput={pool.symbol}
+    swapFee={0.003}
+  >
+    {
+      status === ApprovalStatus.UNKNOWN
+      ? <></>
+      : status === ApprovalStatus.APPROVED
+        ? <Button onClick={handleSubmit} type="primary"> Trade </Button>
+        : <Button onClick={approve} type="primary"> Approve </Button>
+    }
+  </TwoTokenExchangeForm>;
 }
--- src/app/interactions/TradeInteraction.tsx
+++ src/app/interactions/TradeInteraction.tsx
@@ -1,192 +1,106 @@
-import { AiOutlineArrowRight } from "react-icons/ai";
-import { AppState, FormattedIndexPool, selectors } from "features";
-import { COMMON_BASE_TOKENS } from "config";
-import { Flipper, Token, TokenSelector } from "components";
-import { Form, Typography } from "antd";
-import { TokenExchangeRate } from "components/molecules";
+import { AppState, FormattedIndexPool, actions, selectors } from "features";
+import { COMMON_BASE_TOKENS, UNISWAP_ROUTER_ADDRESS } from "config";
 import { Trade } from "@uniswap/sdk";
 import { convert } from "helpers";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
+import { ApprovalStatus } from "features/user/slice";
+import { Button } from "antd";
+import { useTokenApproval } from "./common";
+import { useTokenUserDataListener } from "ethereum/listeners";
 import { useUniswapTradingPairs } from "ethereum/helpers";
+import React, {
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
+import TwoTokenExchangeForm, { TwoTokenExchangeFormValues } from "components/organisms/TwoTokenExchangeForm";
+
 
 interface Props {
   pool: FormattedIndexPool;
 }
 
-type TradeValues = typeof INITIAL_STATE;
-
-const INITIAL_STATE = {
-  from: {
-    token: "DAI",
-    amount: 0,
-  },
-  to: {
-    token: "",
-    amount: 0,
-  },
-};
-
-const { Item } = Form;
-
 export default function TradeInteraction({ pool }: Props) {
-  const [form] = Form.useForm<TradeValues>();
   const [trade, setTrade] = useState<Trade | undefined>();
-  const lastTouchedField = useRef<"input" | "output">("input");
+  const dispatch = useDispatch();
 
-  const handleFlip = () => {
-    const { from, to } = form.getFieldsValue();
-    const flippedValue = {
-      from: to,
-      to: from,
-    };
-
-    form.setFieldsValue(flippedValue);
-  };
   const tokenIds = useMemo(() => {
     return [pool.id, ...COMMON_BASE_TOKENS.map(c => c.id)];
   }, [pool.id]);
+
+  useTokenUserDataListener(UNISWAP_ROUTER_ADDRESS, tokenIds);
 
   const { calculateBestTradeForExactInput, calculateBestTradeForExactOutput } = useUniswapTradingPairs(tokenIds);
   const assets = useSelector((state: AppState) => selectors.selectTokensById(state, tokenIds));
   const tokenLookup = useSelector(selectors.selectTokenLookupBySymbol);
 
-  useEffect(() => {
-    if (form && pool) {
-      const fields = form.getFieldsValue();
-      if (fields && !fields.to.token) {
-        const newFormValues = {
-          to: {
-            token: pool.symbol,
-            amount: 0,
-          },
-        };
-        form.setFieldsValue(newFormValues);
-      }
-    }
-  }, [form, pool]);
+  const handleChange = useCallback(({ from, to, lastTouchedField }: TwoTokenExchangeFormValues) => {
+    const inputToken = tokenLookup[from.token.toLowerCase()];
+    const outputToken = tokenLookup[to.token.toLowerCase()];
 
-  const calculateInputForExactOutput = useCallback((changedValues: TradeValues) => {
-    const { from } = form.getFieldsValue();
-    if (!changedValues.to.token || !from.token) return;
-    let amountIn: number;
-    if (!changedValues.to.amount) {
-      amountIn = 0;
+    if (lastTouchedField === "INPUT") {
+      const typedAmountIn = from.amount;
+      let amountOut = 0;
+      if (typedAmountIn) {
+        const amountIn = convert.toToken(typedAmountIn.toString(), inputToken.decimals).toString(10);
+        const bestTrade = calculateBestTradeForExactInput(inputToken, outputToken, amountIn);
+        setTrade(bestTrade);
+        amountOut = parseFloat(bestTrade?.outputAmount.toFixed(4) ?? "0");
+      }
+      to.amount = amountOut;
     } else {
-      const inputToken = tokenLookup[from.token.toLowerCase()];
-      const outputToken = tokenLookup[changedValues.to.token.toLowerCase()];
-      const amountOut = convert
-        .toToken(changedValues.to.amount.toString(), outputToken.decimals)
-        .toString(10);
-      const bestTrade = calculateBestTradeForExactOutput(
-        inputToken,
-        outputToken,
-        amountOut
-      );
-      setTrade(bestTrade);
-      amountIn = parseFloat(bestTrade?.inputAmount.toFixed(4) ?? "0");
-    }
-
-    form.setFieldsValue({
-      from: {
-        token: from.token,
-        amount: amountIn
+      const typedAmountOut = to.amount;
+      let amountIn = 0;
+      if (typedAmountOut) {
+        const amountOut = convert
+          .toToken(typedAmountOut.toString(), outputToken.decimals)
+          .toString(10);
+        const bestTrade = calculateBestTradeForExactOutput(
+          inputToken,
+          outputToken,
+          amountOut
+        );
+        setTrade(bestTrade);
+        amountIn = parseFloat(bestTrade?.inputAmount.toFixed(4) ?? "0");
       }
-    });
-  }, [calculateBestTradeForExactOutput, form, tokenLookup]);
-
-  const calculateOutputForExactInput = useCallback((changedValues: TradeValues) => {
-    const { from, to } = form.getFieldsValue();
-    if (!changedValues.from.token || !to.token) return;
-    let amountOut: number;
-    if (!changedValues.from.amount) {
-      amountOut = 0;
-    } else {
-      const inputToken = tokenLookup[changedValues.from.token.toLowerCase()];
-      const outputToken = tokenLookup[to.token.toLowerCase()];
-      const amountIn = convert.toToken(changedValues.from.amount.toString(), inputToken.decimals).toString(10);
-      const bestTrade = calculateBestTradeForExactInput(inputToken, outputToken, amountIn);
-      setTrade(bestTrade);
-      amountOut = parseFloat(bestTrade?.outputAmount.toFixed(4) ?? "0");
+      from.amount = amountIn;
     }
-    form.setFieldsValue({
-      to: {
-        token: to.token,
-        amount: amountOut
-      }
-    });
-  }, [calculateBestTradeForExactInput, form, tokenLookup]);
+  }, [tokenLookup, calculateBestTradeForExactInput, calculateBestTradeForExactOutput]);
 
-  const checkAmount = (_: any, value: { amount: number }) => {
-    return value.amount > 0
-      ? Promise.resolve()
-      : Promise.reject("Amount must be greater than zero.");
-  };
+  const [token, amount] = useMemo(() => {
+    if (!trade) return ["", "0"];
+    const token = trade.route.path[0].address;
+    const amount = trade.inputAmount.raw.toString(10)
+    return [token, amount];
+  }, [trade]);
 
-  const [price, fee, baseline, comparison] = useMemo(() => {
-    const fields = form?.getFieldsValue();
-    if (!fields || !fields.from || !fields.to) {
-      return ["0", "0", "", ""];
+  const { status, approve } = useTokenApproval({
+    spender: UNISWAP_ROUTER_ADDRESS,
+    token,
+    amount
+  });
+
+  const handleSubmit = useCallback(() => {
+    if (trade) {
+      dispatch(actions.trade(trade));
     }
-    const baseline = fields.from.token;
-    const comparison = fields.to.token;
-    if (!trade) {
-      return ["0", "0", baseline, comparison];
+  }, [dispatch, trade]);
+
+  return <TwoTokenExchangeForm
+    title={"Trade"}
+    assetOptions={assets as any}
+    onChange={handleChange}
+    defaultInput="DAI"
+    defaultOutput={pool.symbol}
+    swapFee={0.003}
+  >
+    {
+      status === ApprovalStatus.UNKNOWN
+      ? <></>
+      : status === ApprovalStatus.APPROVED
+        ? <Button onClick={handleSubmit} type="primary"> Trade </Button>
+        : <Button onClick={approve} type="primary"> Approve </Button>
     }
-    const price = trade.executionPrice.toFixed(4);
-    const fee = trade.outputAmount
-      .multiply("3")
-      .divide("1000")
-      .toSignificant(5);
-    return [price, fee, baseline, comparison];
-  }, [form, trade]);
-
-  const fromOptions = useMemo(() => {
-    const fields = form.getFieldsValue();
-    if (!fields.to) return assets;
-    return assets.filter((a) => fields.to.token !== a?.symbol);
-  }, [assets, form]);
-
-  const toOptions = useMemo(() => {
-    const fields = form.getFieldsValue();
-    if (!fields.from) return assets;
-    return assets.filter((a) => fields.from.token !== a?.symbol);
-  }, [assets, form]);
-
-  return (
-    <Form
-      form={form}
-      initialValues={INITIAL_STATE}
-      onValuesChange={(changedValues) => {
-        if (changedValues.from) {
-          lastTouchedField.current = "input";
-          calculateOutputForExactInput(changedValues);
-        } else if (changedValues.to) {
-          lastTouchedField.current = "output";
-          calculateInputForExactOutput(changedValues);
-        }
-      }}
-    >
-      <Typography.Title>
-        <span>Trade</span>
-        {baseline && comparison && (
-          <span>
-            <Token name="Baseline" image={baseline} />
-            <AiOutlineArrowRight />
-            <Token name="Comparison" image={comparison} />
-          </span>
-        )}
-      </Typography.Title>
-      <Item name="from" rules={[{ validator: checkAmount }]}>
-        {pool && <TokenSelector label="From" assets={fromOptions as any} />}
-      </Item>
-      <Flipper onFlip={handleFlip} />
-      <Item name="to" rules={[{ validator: checkAmount }]}>
-        {pool && <TokenSelector label="To" assets={toOptions as any} />}
-      </Item>
-      <Item>
-        <TokenExchangeRate {...{ baseline, comparison, fee, rate: price }} />
-      </Item>
-    </Form>
-  );
+  </TwoTokenExchangeForm>;
 }
