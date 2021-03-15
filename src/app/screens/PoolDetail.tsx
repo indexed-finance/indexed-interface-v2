f087d6a1c8c88581085f7fa30c3fe9c7b30d604c
d1ll0n <dillonkellar@protonmail.com>
2021-03-15 14:05:36 -0500

    Update usePoolDataListener import

--- src/app/screens/PoolDetail.tsx
+++ src/app/screens/PoolDetail.tsx
@@ -1,4 +1,4 @@
-import { AppState, hooks, selectors } from "features";
+import { AppState, selectors } from "features";
 import {
   ChartCard,
   PoolDropdown,
@@ -12,9 +12,8 @@ import { Performance, Recent, Subscreen } from "app/subscreens";
 import { PoolInteractions } from "app/interactions";
 import { Redirect, useParams } from "react-router-dom";
 import { useBreakpoints } from "helpers";
+import { usePoolDataListener } from "features/batcher/hooks";
 import { useSelector } from "react-redux";
-
-const { useDataListener } = hooks;
 
 export default function PoolDetail() {
   const { poolName } = useParams<{ poolName: string }>();
@@ -35,7 +34,7 @@ function Pool({ id }: { id: string }) {
   );
   const breakpoints = useBreakpoints();
 
-  useDataListener("TokenUserData", id, tokenIds);
+  usePoolDataListener(id, tokenIds);
 
   if (pool) {
     // Subscreens
