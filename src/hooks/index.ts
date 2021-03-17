b28917ec173e69c880ebd1969b73f5cda5dd3e70
d1ll0n <dillonkellar@protonmail.com>
2021-03-17 17:24:38 -0500

    add burn callbacks hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,3 +4,5 @@ export { default as useTokenApproval } from "./use-token-approval";
 export { default as useTokenRandomizer } from "./use-token-randomizer";
 export { default as useUniswapTradingPairs } from "./use-uniswap-trading-pairs";
 export { default as useMintRouterCallbacks } from "./use-mint-router-callbacks";
+export { useSingleTokenBurnCallbacks } from "./use-burn-callbacks";
+export { useSingleTokenMintCallbacks } from "./use-mint-callbacks";
