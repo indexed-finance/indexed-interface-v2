7026851830ee9ee4657392ab9d83e1ff18a2db58
d1ll0n <dillonkellar@protonmail.com>
2021-03-17 08:12:59 -0500

    added uniswap minting interface

--- src/app/interactions/MintInteraction.tsx
+++ src/app/interactions/MintInteraction.tsx
@@ -1,25 +1,40 @@
 import { AppState, FormattedIndexPool, selectors } from "features";
-import { SLIPPAGE_RATE, UNISWAP_ROUTER_ADDRESS } from "config";
+import { Fragment, useCallback, useState } from "react";
+import { MINT_ROUTER_ADDRESS, SLIPPAGE_RATE, UNISWAP_ROUTER_ADDRESS } from "config";
+import { Radio } from "antd";
 import { convert } from "helpers";
 import { downwardSlippage, upwardSlippage } from "ethereum";
-import { useCallback } from "react";
 import { useSelector } from "react-redux";
 import { useSingleTokenMintCallbacks } from "hooks/use-mint-callbacks";
 import { useTokenUserDataListener } from "features/batcher/hooks";
+import { useTokens } from "features/tokens/hooks";
 import BaseInteraction, { InteractionValues } from "./BaseInteraction";
 import BigNumber from "bignumber.js";
+import useMintRouterCallbacks from "hooks/use-mint-router-callbacks";
 
 interface Props {
   pool: FormattedIndexPool;
 }
 
-export default function SingleTokenMintInteraction({ pool }: Props) {
+export default function MintInteraction({ pool }: Props) {
+  const [mintType, setMintType] = useState<"single" | "uniswap" | "multi">("single");
+  return <Fragment>
+    <Radio.Group value={mintType} onChange={({ target: { value } }) => {setMintType(value)}}>
+      <Radio.Button value="single">Single Input</Radio.Button>
+      <Radio.Button value="multi">Multi Input</Radio.Button>
+      <Radio.Button value="uniswap">Uniswap</Radio.Button>
+    </Radio.Group>
+    { mintType === "single" && <SingleTokenMintInteraction pool={pool} /> }
+    { mintType === "uniswap" && <UniswapMintInteraction pool={pool} /> }
+  </Fragment>
+}
+
+function SingleTokenMintInteraction({ pool }: Props) {
   const tokenLookup = useSelector(selectors.selectTokenLookupBySymbol);
   const tokenIds = useSelector((state: AppState) => [
     ...selectors.selectPoolTokenIds(state, pool.id),
     pool.id,
   ]);
-  useTokenUserDataListener(pool.id, tokenIds);
   const {
     calculateAmountIn,
     calculateAmountOut,
@@ -120,6 +135,96 @@ export default function SingleTokenMintInteraction({ pool }: Props) {
       onSubmit={handleSubmit}
       onChange={handleChange}
       defaultOutputSymbol={pool.symbol}
+      disableOutputSelect
+    />
+  );
+}
+
+
+function UniswapMintInteraction({ pool }: Props) {
+  const tokenLookup = useSelector(selectors.selectTokenLookupBySymbol);
+  const {
+    tokenIds,
+    getBestMintRouteForAmountIn,
+    getBestMintRouteForAmountOut
+  } = useMintRouterCallbacks(pool.id);
+
+  const assets = useTokens(tokenIds);
+  useTokenUserDataListener(MINT_ROUTER_ADDRESS, tokenIds);
+
+  const handleChange = useCallback(
+    (values: InteractionValues) => {
+      const {
+        fromToken,
+        fromAmount,
+        toToken,
+        toAmount,
+        lastTouchedField,
+      } = values;
+
+      if (!toToken || !fromToken) {
+        return;
+      }
+      if (lastTouchedField === "from") {
+        if (!fromAmount || isNaN(fromAmount) || fromAmount < 0) {
+          values.fromAmount = 0;
+          values.toAmount = 0;
+          return;
+        }
+        const result = getBestMintRouteForAmountIn(fromToken, fromAmount.toString());
+        if (result) {
+          if (result.poolResult?.error) {
+            return result.poolResult.error;
+          } else {
+            const { decimals } = tokenLookup[toToken.toLowerCase()];
+            values.toAmount = parseFloat(
+              convert.toBalance(
+                downwardSlippage(
+                  result.poolResult.poolAmountOut as BigNumber,
+                  SLIPPAGE_RATE
+                ),
+                decimals
+              )
+            );
+          }
+        }
+      } else {
+        if (!toAmount || isNaN(toAmount) || toAmount < 0) {
+          values.fromAmount = 0;
+          values.toAmount = 0;
+          return;
+        }
+
+        const result = getBestMintRouteForAmountOut(fromToken, toAmount.toString());
+        if (result) {
+          if (result.poolResult?.error) {
+            return result.poolResult.error;
+          } else {
+            values.fromAmount = parseFloat(result.uniswapResult.outputAmount.toFixed(4));
+          }
+        }
+      }
+    },
+    [getBestMintRouteForAmountIn, getBestMintRouteForAmountOut, tokenLookup]
+  );
+
+  const handleSubmit = useCallback(
+    (values: InteractionValues) => {
+      console.log(values)
+
+    },
+    []
+  );
+
+  return (
+    <BaseInteraction
+      title="Mint with Uniswap"
+      assets={assets.filter(_ => _) as { name: string; symbol: string; id: string }[]}
+      spender={pool.id}
+      onSubmit={handleSubmit}
+      onChange={handleChange}
+      defaultOutputSymbol={pool.symbol}
+      disableOutputSelect
     />
   );
 }
