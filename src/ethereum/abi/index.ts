6ed400dee02114969c58009bdf1d03e20d50ed29
d1ll0n <dillonkellar@protonmail.com>
2021-03-19 01:03:39 -0500

    add mint and burn router abis

--- src/ethereum/abi/index.ts
+++ src/ethereum/abi/index.ts
@@ -3,6 +3,8 @@ import { Interface } from "ethers/lib/utils";
 import IERC20_ABI from "./IERC20.json";
 import IPoolInitializer_ABI from "./IPoolInitializer.json";
 import IPool_ABI from "./IPool.json";
+import IndexedUniswapRouterBurner_ABI from "./IndexedUniswapRouterBurner.json";
+import IndexedUniswapRouterMinter_ABI from "./IndexedUniswapRouterMinter.json";
 import Pair_ABI from "./Pair.json";
 import StakingRewardsFactory_ABI from "./StakingRewardsFactory.json";
 import StakingRewards_ABI from "./StakingRewards.json";
@@ -15,6 +17,8 @@ export const Pair = new Interface(Pair_ABI);
 export const StakingRewards = new Interface(StakingRewards_ABI);
 export const StakingRewardsFactory = new Interface(StakingRewardsFactory_ABI);
 export const UniswapV2Router = new Interface(UniswapV2Router_ABI);
+export const IndexedUniswapRouterBurner = new Interface(IndexedUniswapRouterBurner_ABI);
+export const IndexedUniswapRouterMinter = new Interface(IndexedUniswapRouterMinter_ABI);
 
 export {
   IPool_ABI,
@@ -23,5 +27,7 @@ export {
   Pair_ABI,
   StakingRewards_ABI,
   StakingRewardsFactory_ABI,
-  UniswapV2Router_ABI
+  UniswapV2Router_ABI,
+  IndexedUniswapRouterBurner_ABI,
+  IndexedUniswapRouterMinter_ABI
 };
