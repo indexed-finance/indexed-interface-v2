242e416eea25184ad0cf9466634be86d6b963793
d1ll0n <dillonkellar@protonmail.com>
2021-03-26 23:56:09 -0500

    fix return types to conform with code style

--- src/hooks/use-token-price.ts
+++ src/hooks/use-token-price.ts
@@ -2,13 +2,13 @@ import { NDX_ADDRESS, WETH_CONTRACT_ADDRESS } from "config";
 import { actions, selectors, usePricesRegistrar, useToken } from "features"
 
 
-export function useTokenPrice(id: string): [true, number] | [false, undefined] {
+export function useTokenPrice(id: string): [number, false] | [undefined, true] {
   const token = useToken(id.toLowerCase());
   usePricesRegistrar([id], actions, selectors);
   if (token?.priceData?.price) {
-    return [true, token.priceData.price];
+    return [token.priceData.price, false];
   }
-  return [false, undefined];
+  return [undefined, true];
 }
 
 export const useEthPrice = () => useTokenPrice(WETH_CONTRACT_ADDRESS);
