7d32d47900cbada8c9aeb3ff998d8b50ada8c745
d1ll0n <dillonkellar@protonmail.com>
2021-04-15 08:24:24 -0500

    add duration helpers

---
+++ src/helpers/time.ts
@@ -0,0 +1,10 @@
+export const durationIn = {
+  seconds: (n: number, unit: 'ms' | 's' = 'ms') => n * (unit === 'ms' ? 1000 : 1),
+  minutes: (n: number, unit: 'ms' | 's' = 'ms') => n * durationIn.seconds(60, unit),
+  hours: (n: number, unit: 'ms' | 's' = 'ms') => n * durationIn.minutes(60, unit),
+  days: (n: number, unit: 'ms' | 's' = 'ms') => n * durationIn.hours(24, unit),
+  weeks: (n: number, unit: 'ms' | 's' = 'ms') => n * durationIn.days(7, unit),
+  years: (n: number, unit: 'ms' | 's' = 'ms') => n * durationIn.days(365, unit),
+}
+
+export default durationIn; 
