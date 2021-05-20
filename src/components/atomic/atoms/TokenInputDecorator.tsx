51e4cdcaa3d40ca3d302675458a7ce55310cbadf
d1ll0n <dillonkellar@protonmail.com>
2021-05-19 22:40:02 -0500

    separate token input decorator into its own component

---
+++ src/components/atomic/atoms/TokenInputDecorator.tsx
@@ -0,0 +1,45 @@
+import { Button, Typography } from "antd";
+import { useTranslator } from "hooks";
+
+type Props = {
+  showBalance: boolean;
+  balance?: number;
+  error?: string;
+  onClickMax?: () => void;
+}
+
+export function TokenInputDecorator({
+  showBalance,
+  balance = 0,
+  error,
+  onClickMax
+}: Props) {
+  const tx = useTranslator();
+  if (error) {
+    return <Typography.Text
+      type="warning"
+      style={{ textAlign: "left", color: 'red' }}
+    > {error} </Typography.Text>
+  }
+  if (!showBalance) return null;
+
+  return <Typography.Text
+    type="secondary"
+    style={{ textAlign: "left" }}
+  >
+    {balance > 0 ? (
+      <>
+        Balance: {balance}{" "}
+        <Button
+            type="text"
+            onClick={onClickMax}
+            style={{ fontSize: 12 }}
+          >
+            {tx("MAX")}
+          </Button>
+      </>
+    ) : (
+      tx("NO_BALANCE")
+    )}
+  </Typography.Text>
+}
