75efd5f9836e0ff57e373823cbe9a22f821e6c47
Outrun Colors, LLC <outruncolors@gmail.com>
2021-05-21 21:41:53 -0500

    Setup for the interactive staking widget

--- src/components/atomic/organisms/StakingWidget.tsx
+++ src/components/atomic/organisms/StakingWidget.tsx
@@ -1,9 +1,10 @@
 import { AppState, FormattedStakingData, selectors } from "features";
-import { Badge, Button, Space, Statistic } from "antd";
+import { Badge, Button, Drawer, Space, Statistic } from "antd";
 import { FaTractor } from "react-icons/fa";
-import { Link, useHistory } from "react-router-dom";
+import { StakeInteraction } from "components/interactions";
 import { Widget } from "./Widget";
 import { convert } from "helpers";
+import { useHistory } from "react-router-dom";
 import {
   usePoolDetailRegistrar,
   useStakingApy,
@@ -11,6 +12,7 @@ import {
   useTranslator,
 } from "hooks";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export function StakingWidget(props: FormattedStakingData) {
   const tx = useTranslator();
@@ -32,52 +34,76 @@ export function StakingWidget(props: FormattedStakingData) {
       : []
   );
   const { push } = useHistory();
+  const [showingStakingForm, setShowingStakingForm] = useState(false);
   const inner = (
-    <Widget
-      symbol={symbol}
-      address={props.id}
-      price={price ? convert.toCurrency(price) : ""}
-      onClick={() =>
-        props.isWethPair
-          ? window.open(
-              `https://info.uniswap.org/pair/${props.stakingToken.toLowerCase()}`
-            )
-          : push(props.slug)
-      }
-      stats={
-        <Space direction="vertical">
-          <Statistic title={tx("STAKED")} value={`${props.staked} ${symbol}`} />
-          <Statistic title={tx("EARNED")} value={props.earned} />
-          <Statistic
-            title={tx("APY")}
-            value={apy ?? "Expired"}
-            valueStyle={{ color: isExpired ? "#333" : "inherit" }}
-          />
-          <Statistic
-            title={tx("RATE")}
-            value={isExpired ? "Expired" : props.rate}
-            valueStyle={{ color: isExpired ? "#333" : "inherit" }}
-          />
-        </Space>
-      }
-      actions={
-        <Button
-          type={isExpired ? "ghost" : "primary"}
-          disabled={isExpired}
-          size="large"
-          onClick={(event) => event.stopPropagation()}
+    <div style={{ position: "relative" }}>
+      <Widget
+        symbol={symbol}
+        address={props.id}
+        price={price ? convert.toCurrency(price) : ""}
+        onClick={() =>
+          props.isWethPair
+            ? window.open(
+                `https://info.uniswap.org/pair/${props.stakingToken.toLowerCase()}`
+              )
+            : push(props.slug)
+        }
+        stats={
+          <Space direction="vertical">
+            <Statistic
+              title={tx("STAKED")}
+              value={`${props.staked} ${symbol}`}
+            />
+            <Statistic title={tx("EARNED")} value={props.earned} />
+            <Statistic
+              title={tx("APY")}
+              value={apy ?? "Expired"}
+              valueStyle={{ color: isExpired ? "#333" : "inherit" }}
+            />
+            <Statistic
+              title={tx("RATE")}
+              value={isExpired ? "Expired" : props.rate}
+              valueStyle={{ color: isExpired ? "#333" : "inherit" }}
+            />
+          </Space>
+        }
+        actions={
+          <>
+            <Button
+              type={isExpired ? "ghost" : "primary"}
+              disabled={isExpired}
+              size="large"
+              onClick={(event) => {
+                event.stopPropagation();
+                setShowingStakingForm(true);
+              }}
+            >
+              {formattedIndexPool && (
+                  <Space>
+                    <FaTractor style={{ position: "relative", top: 2 }} />
+                    <span>
+                      {isExpired ? tx("STAKING_EXPIRED") : tx("STAKE")}
+                    </span>
+                  </Space>
+              )}
+            </Button>
+          </>
+        }
+      />
+      {formattedIndexPool && showingStakingForm && (
+        <Drawer
+          getContainer={false}
+          style={{ position: "absolute", left: 0, top: 75}}
+          width="98%"
+          placement="right"
+          visible={true}
+          closable={true}
+          onClose={() => setShowingStakingForm(false)}
         >
-          {formattedIndexPool && (
-            <Link to={`${formattedIndexPool.slug}?interaction=stake`}>
-              <Space>
-                <FaTractor style={{ position: "relative", top: 2 }} />
-                <span>{isExpired ? tx("STAKING_EXPIRED") : tx("STAKE")}</span>
-              </Space>
-            </Link>
-          )}
-        </Button>
-      }
-    />
+          <StakeInteraction indexPool={formattedIndexPool} />
+        </Drawer>
+      )}
+    </div>
   );
 
   usePoolDetailRegistrar(formattedIndexPool?.id ?? "", tokenIds);
