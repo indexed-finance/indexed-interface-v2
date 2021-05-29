1eb2401602487555329a1ecd87b9cc68793ae6b6
d1ll0n <dillonkellar@protonmail.com>
2021-05-29 02:30:12 -0500

    Add generated hooks

--- src/hooks/contract-hooks.ts
+++ src/hooks/contract-hooks.ts
@@ -1,40 +1,60 @@
 import { BURN_ROUTER_ADDRESS, MINT_ROUTER_ADDRESS, MULTI_TOKEN_STAKING_ADDRESS, UNISWAP_ROUTER_ADDRESS } from "config";
-import { Contract } from "@ethersproject/contracts";
-import { IERC20, IPool, IndexedUniswapRouterBurner, IndexedUniswapRouterMinter, MultiTokenStaking, StakingRewards, UniswapV2Router } from "ethereum";
-import { Interface, JsonFragment } from "@ethersproject/abi";
+
+import { ContractTypeLookup, InterfaceKind, getContract } from "ethereum";
+
 import { useSigner } from "features";
 
-export function useContractWithSigner(address: string, abi: Interface | JsonFragment[]): Contract | undefined {
+export function useContractWithSigner<T extends InterfaceKind>(address: string, name: T): ContractTypeLookup[T] | undefined {
   const signer = useSigner();
   if (signer && address) {
-    return new Contract(address, abi, signer);
+    return getContract(address, name, signer);
   }
 }
 
 export function useTokenContract(address: string) {
-  return useContractWithSigner(address, IERC20);
+	return useContractWithSigner(address, "IERC20");
 }
 
-export function useUniswapRouterContract() {
-  return useContractWithSigner(UNISWAP_ROUTER_ADDRESS, UniswapV2Router);
-}
-
-export function useIndexPoolContract(address: string) {
-  return useContractWithSigner(address, IPool);
+export function useIIndexedUniswapV2OracleContract(address: string) {
+	return useContractWithSigner(address, "IIndexedUniswapV2Oracle");
 }
 
 export function useBurnRouterContract() {
-  return useContractWithSigner(BURN_ROUTER_ADDRESS, IndexedUniswapRouterBurner);
+	return useContractWithSigner(BURN_ROUTER_ADDRESS, "IndexedUniswapRouterBurner");
 }
 
 export function useMintRouterContract() {
-  return useContractWithSigner(MINT_ROUTER_ADDRESS, IndexedUniswapRouterMinter);
+	return useContractWithSigner(MINT_ROUTER_ADDRESS, "IndexedUniswapRouterMinter");
 }
 
-export function useStakingRewardsContract(address: string) {
-  return useContractWithSigner(address, StakingRewards);
+export function useIndexPoolContract(address: string) {
+	return useContractWithSigner(address, "IPool");
+}
+
+export function useIPoolInitializerContract(address: string) {
+	return useContractWithSigner(address, "IPoolInitializer");
 }
 
 export function useMultiTokenStakingContract() {
-  return useContractWithSigner(MULTI_TOKEN_STAKING_ADDRESS, MultiTokenStaking);
+	return useContractWithSigner(MULTI_TOKEN_STAKING_ADDRESS, "MultiTokenStaking");
+}
+
+export function usePairContract(address: string) {
+	return useContractWithSigner(address, "Pair");
+}
+
+export function useRewardsScheduleContract(address: string) {
+	return useContractWithSigner(address, "RewardsSchedule");
+}
+
+export function useStakingRewardsContract(address: string) {
+	return useContractWithSigner(address, "StakingRewards");
+}
+
+export function useStakingRewardsFactoryContract(address: string) {
+	return useContractWithSigner(address, "StakingRewardsFactory");
+}
+
+export function useUniswapRouterContract() {
+	return useContractWithSigner(UNISWAP_ROUTER_ADDRESS, "UniswapV2Router");
 }
