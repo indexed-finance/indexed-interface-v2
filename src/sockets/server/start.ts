db715af6ba3efa9d30a4421b1e5ac4c297a04a6a
Outrun Colors, LLC <outruncolors@gmail.com>
2021-06-01 12:56:57 -0500

    Ignore me.

--- src/sockets/server/start.ts
+++ src/sockets/server/start.ts
@@ -2,7 +2,6 @@ import { log } from "./helpers";
 import { setupClientHandling } from "./client-handling";
 import { setupHealthChecks } from "./health-check";
 import { setupLogHandling } from "./log-handling";
-import { setupProxy } from "./proxy-handling";
 import { setupStateHandling } from "./state-handling";
 
 log("SocketServer has started up!");
@@ -10,5 +9,4 @@ log("SocketServer has started up!");
 setupClientHandling();
 setupStateHandling();
 setupLogHandling();
-setupProxy();
 setupHealthChecks();
