0234b42f2bf40f29f6317a9bfb1e76ba18a0b4d8
d1ll0n <dillonkellar@protonmail.com>
2021-06-03 02:03:36 -0500

    register calls even if connected

--- src/hooks/use-call-registrar.ts
+++ src/hooks/use-call-registrar.ts
@@ -1,12 +1,11 @@
-import { DataReceiverConfig, actions, selectors } from "features";
+import { DataReceiverConfig, actions } from "features";
 import { isEqual } from "lodash";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 
 export function useCallRegistrar(calls: DataReceiverConfig) {
   const dispatch = useDispatch();
   const [cachedCalls, setCachedCalls] = useState(calls);
-  const isConnected = useSelector(selectors.selectConnected);
 
   // Effect:
   // Handle updates "gracefully" in a McJanky way.
@@ -20,7 +19,7 @@ export function useCallRegistrar(calls: DataReceiverConfig) {
   // Register a multicall listener that queries certain functions every update.
   // Note: if the user is connected, defer all updates to the server.
   useEffect(() => {
-    if (!isConnected && cachedCalls) {
+    if (cachedCalls) {
       const allCalls = {
         caller: cachedCalls.caller,
         onChainCalls: cachedCalls.onChainCalls ?? [],
@@ -33,5 +32,5 @@ export function useCallRegistrar(calls: DataReceiverConfig) {
         dispatch(actions.callsUnregistered(allCalls));
       };
     }
-  }, [dispatch, isConnected, cachedCalls]);
+  }, [dispatch, cachedCalls]);
 }
