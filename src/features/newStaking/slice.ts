ce5c4d7c274ab92c4666c977424540daa25c24f0
Outrun Colors, LLC <outruncolors@gmail.com>
2021-06-05 14:00:26 -0500

    Incorporate new staking on portfolio page

--- src/features/newStaking/slice.ts
+++ src/features/newStaking/slice.ts
@@ -104,16 +104,28 @@ export const newStakingSelectors = {
       .selectAllNewStakingPools(state)
       .find(({ token }) => token.toLowerCase() === id.toLowerCase());
   },
-  selectNewStakingPoolsByStakingTokens(state: AppState, ids: string[]) {
-    const allPools = newStakingSelectors.selectAllNewStakingPools(state);
-
-    return ids.map((id) =>
-      allPools.find((p) => p.token.toLowerCase() === id.toLowerCase())
-    );
-  },
   selectNewUserStakedBalance(state: AppState, id: string) {
     return newStakingSelectors.selectNewStakingPool(state, id)
       ?.userStakedBalance;
+  },
+  selectNewStakingInfoLookup(state: AppState, ids: string[]) {
+    return ids.reduce((prev, next) => {
+      const pool = newStakingSelectors.selectNewStakingPoolByStakingToken(
+        state,
+        next
+      );
+
+      if (pool) {
+        const { userStakedBalance = "0", userEarnedRewards = "0" } = pool;
+
+        prev[pool.token] = {
+          balance: userStakedBalance.toString(),
+          rewards: userEarnedRewards.toString(),
+        };
+      }
+
+      return prev;
+    }, {} as Record<string, { balance: string; rewards: string }>);
   },
 };
 
