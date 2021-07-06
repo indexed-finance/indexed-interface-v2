1a67f779980628dfe8d72b05f71774227b13b98d
Outrun Colors, LLC <outruncolors@gmail.com>
2021-07-05 19:49:52 -0500

    Add fake adapter data

--- src/features/vaults/slice.ts
+++ src/features/vaults/slice.ts
@@ -25,11 +25,28 @@ const slice = createSlice({
         } else {
           adapter.upsertMany(state, [
             {
-              id: "123",
+              id: "0x6b175474e89094c44da98b954eedeac495271d0f",
               symbol: "FAKE",
               name: "Fake Vault",
               underlying: "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
-              adapters: [],
+              adapters: [
+                {
+                  id: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  underlying: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  token: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  name: "Compound FAKE Adapter",
+                  apr: 0.05,
+                  protocolID: "compound",
+                },
+                {
+                  id: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  underlying: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  token: "0x6b175474e89094c44da98b954eedeac495271d0f",
+                  name: "AAVE FAKE Adapter",
+                  apr: 0.05,
+                  protocolID: "aave",
+                },
+              ],
               weights: ["5e17", "5e17"],
               balances: ["0"],
               netAPR: 0.05,
