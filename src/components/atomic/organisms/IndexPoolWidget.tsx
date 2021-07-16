dfa7d49c406b71b0521c89b0d5a16866258758cf
Outrun Colors, LLC <outruncolors@gmail.com>
2021-07-16 14:54:09 -0500

    Create animations directory and Fade

--- src/components/atomic/organisms/IndexPoolWidget.tsx
+++ src/components/atomic/organisms/IndexPoolWidget.tsx
@@ -1,20 +1,33 @@
 import { AppState, FormattedIndexPool, selectors } from "features";
 import { Card, Col, Row, Statistic } from "antd";
+import { Fade } from "components/animations";
 import { INDEX_POOL_TAGLINES } from "config";
 import { Quote } from "../molecules";
 import { Token } from "../atoms";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { usePoolDetailRegistrar } from "hooks";
 import { useSelector } from "react-redux";
 
 export function IndexPoolWidgetGroup() {
+  const [fadedWidget, setFadedWidget] = useState(-1);
   const indexPools = useSelector(selectors.selectAllFormattedIndexPools);
+
+  useEffect(() => {
+    if (fadedWidget < indexPools.length - 1) {
+      setTimeout(() => {
+        setFadedWidget((prev) => prev + 1);
+      }, 200);
+    }
+  }, [fadedWidget, indexPools]);
 
   return (
     <Row gutter={[20, 20]}>
-      {indexPools.map((pool) => (
+      {indexPools.map((pool, index) => (
         <Col xs={24} sm={8} key={pool.id}>
-          <IndexPoolWidget {...pool} />
+          <Fade in={fadedWidget >= index}>
+            <IndexPoolWidget {...pool} />
+          </Fade>
         </Col>
       ))}
     </Row>
