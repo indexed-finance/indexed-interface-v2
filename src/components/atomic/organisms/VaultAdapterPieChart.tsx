9d16c70d0c0c747fc24d827d5146027e6d95961b
d1ll0n <dillonkellar@protonmail.com>
2021-08-03 02:27:03 -0500

    Update pie chart

--- src/components/atomic/organisms/VaultAdapterPieChart.tsx
+++ src/components/atomic/organisms/VaultAdapterPieChart.tsx
@@ -1,22 +1,13 @@
 // Taken from https://recharts.org/en-US/examples/CustomActiveShapePieChart
-import { Pie, PieChart, ResponsiveContainer, Sector } from "recharts";
+import { Pie, PieChart, ResponsiveContainer, Sector, Tooltip } from "recharts";
 import { PureComponent } from "react";
 
 const RADIAN = Math.PI / 180;
 
+
 const renderActiveShape = (props: any) => {
-  const {
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    startAngle,
-    endAngle,
-    fill,
-    payload,
-    value,
-  } = props;
+  const RADIAN = Math.PI / 260;
+  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload,  apr, value } = props;
   const sin = Math.sin(-RADIAN * midAngle);
   const cos = Math.cos(-RADIAN * midAngle);
   const sx = cx + (outerRadius + 10) * cos;
@@ -25,7 +16,7 @@ const renderActiveShape = (props: any) => {
   const my = cy + (outerRadius + 30) * sin;
   const ex = mx + (cos >= 0 ? 1 : -1) * 22;
   const ey = my;
-  const textAnchor = cos >= 0 ? "start" : "end";
+  const textAnchor = cos >= 0 ? 'start' : 'end';
 
   return (
     <g>
@@ -50,63 +41,70 @@ const renderActiveShape = (props: any) => {
         outerRadius={outerRadius + 10}
         fill={fill}
       />
-      <path
-        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
-        stroke={fill}
-        fill="none"
-      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        textAnchor={textAnchor}
-        fill="#fff"
-      >{`${value}% of total`}</text>
-      <text
-        x={ex + (cos >= 0 ? 1 : -1) * 12}
-        y={ey}
-        dy={18}
-        textAnchor={textAnchor}
-        fill="#38EE7A"
-      >
-        APR: {payload.apr}
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#888">{`Weight ${value}%`}</text>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+        {`APR ${apr}%`}
       </text>
     </g>
   );
 };
 
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  payload,
-  index,
-}: any) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-    >
-      {payload.name}
-    </text>
-  );
-};
-
 interface Props {
-  data: Array<{ name: string; value: number; apr: string }>;
+  data: Array<{ name: string; weight: number; }>;
+}
+export class VaultAdapterPieChart extends PureComponent<Props> {
+  state = {
+    activeIndex: 0,
+  };
+
+  onPieEnter = (_: any, index: number) => {
+    this.setState({
+      activeIndex: index,
+    });
+  };
+
+  render() {
+    return (
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            activeIndex={this.state.activeIndex}
+            activeShape={renderActiveShape}
+            data={this.props.data}
+            cx="50%"
+            cy="50%"
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+            onMouseEnter={this.onPieEnter}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+      // <ResponsiveContainer width="100%" height="100%">
+      //   <PieChart width={400} height={400}>
+      //     <Pie
+      //       activeIndex={this.state.activeIndex}
+      //       dataKey="value"
+      //       isAnimationActive={false}
+      //       data={this.props.data}
+      //       cx="50%"
+      //       cy="50%"
+      //       outerRadius={80}
+      //       fill="#8884d8"
+      //       // label={renderCustomizedLabel}
+      //     />
+      //     <Tooltip content={<CustomTooltip />} />
+      //   </PieChart>
+      // </ResponsiveContainer>
+    );
+  }
 }
 
-export class VaultAdapterPieChart extends PureComponent<Props> {
+
+/* extends PureComponent<Props> {
   static demoUrl =
     "https://codesandbox.io/s/pie-chart-with-customized-active-shape-y93si";
 
@@ -141,4 +139,4 @@ export class VaultAdapterPieChart extends PureComponent<Props> {
       </ResponsiveContainer>
     );
   }
-}
+} */
