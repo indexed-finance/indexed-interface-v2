e5a019c53fe4164c0ce6fd23a56c25798148b1bc
Outrun Colors, LLC <outruncolors@gmail.com>
2021-08-17 18:30:42 -0500

    Local version of walletconnect in the mean time

--- src/ethereum/connectors/WalletConnect.ts
+++ src/ethereum/connectors/WalletConnect.ts
@@ -1,5 +1,6 @@
 import { INFURA_ID } from "config";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+// import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { WalletConnectConnector } from "libs/@walletconnect-connector";
 
 export const walletConnect = new WalletConnectConnector({
   infuraId: INFURA_ID,
