d9ea960c802e2a2ff4decc43075e47ace3d1c09d
d1ll0n <dillonkellar@protonmail.com>
2021-09-18 20:51:12 -0500

    add dndx token and timelock abis

--- src/ethereum/abi/interfaces.ts
+++ src/ethereum/abi/interfaces.ts
@@ -2,6 +2,7 @@ import { Interface } from "ethers/lib/utils";
 
 import type {
 	AdapterRegistry,
+	ERC20DividendsOwned,
 	Erc20Adapter,
 	IERC20,
 	IIndexedUniswapV2Oracle,
@@ -16,12 +17,14 @@ import type {
 	NirnVault,
 	Pair,
 	RewardsSchedule,
+	SharesTimeLock,
 	StakingRewards,
 	StakingRewardsFactory,
 	UniswapV2Router,
 } from "./types";
 
 import AdapterRegistry_ABI from "./AdapterRegistry.json";
+import ERC20DividendsOwned_ABI from "./ERC20DividendsOwned.json";
 import Erc20Adapter_ABI from "./Erc20Adapter.json";
 import IERC20_ABI from "./IERC20.json";
 import IIndexedUniswapV2Oracle_ABI from "./IIndexedUniswapV2Oracle.json";
@@ -36,12 +39,14 @@ import MultiTokenStaking_ABI from "./MultiTokenStaking.json";
 import NirnVault_ABI from "./NirnVault.json";
 import Pair_ABI from "./Pair.json";
 import RewardsSchedule_ABI from "./RewardsSchedule.json";
+import SharesTimeLock_ABI from "./SharesTimeLock.json";
 import StakingRewardsFactory_ABI from "./StakingRewardsFactory.json";
 import StakingRewards_ABI from "./StakingRewards.json";
 import UniswapV2Router_ABI from "./UniswapV2Router.json";
 
 export const AdapterRegistryInterface = new Interface(AdapterRegistry_ABI) as AdapterRegistry["interface"];
 export const Erc20AdapterInterface = new Interface(Erc20Adapter_ABI) as Erc20Adapter["interface"];
+export const ERC20DividendsOwnedInterface = new Interface(ERC20DividendsOwned_ABI) as ERC20DividendsOwned["interface"];
 export const IERC20Interface = new Interface(IERC20_ABI) as IERC20["interface"];
 export const IIndexedUniswapV2OracleInterface = new Interface(IIndexedUniswapV2Oracle_ABI) as IIndexedUniswapV2Oracle["interface"];
 export const IndexedNarwhalRouterInterface = new Interface(IndexedNarwhalRouter_ABI) as IndexedNarwhalRouter["interface"];
@@ -55,6 +60,7 @@ export const MultiTokenStakingInterface = new Interface(MultiTokenStaking_ABI) a
 export const NirnVaultInterface = new Interface(NirnVault_ABI) as NirnVault["interface"];
 export const PairInterface = new Interface(Pair_ABI) as Pair["interface"];
 export const RewardsScheduleInterface = new Interface(RewardsSchedule_ABI) as RewardsSchedule["interface"];
+export const SharesTimeLockInterface = new Interface(SharesTimeLock_ABI) as SharesTimeLock["interface"];
 export const StakingRewardsInterface = new Interface(StakingRewards_ABI) as StakingRewards["interface"];
 export const StakingRewardsFactoryInterface = new Interface(StakingRewardsFactory_ABI) as StakingRewardsFactory["interface"];
 export const UniswapV2RouterInterface = new Interface(UniswapV2Router_ABI) as UniswapV2Router["interface"];
@@ -62,6 +68,7 @@ export const UniswapV2RouterInterface = new Interface(UniswapV2Router_ABI) as Un
 export {
 	AdapterRegistry_ABI,
 	Erc20Adapter_ABI,
+	ERC20DividendsOwned_ABI,
 	IERC20_ABI,
 	IIndexedUniswapV2Oracle_ABI,
 	IndexedNarwhalRouter_ABI,
@@ -75,6 +82,7 @@ export {
 	NirnVault_ABI,
 	Pair_ABI,
 	RewardsSchedule_ABI,
+	SharesTimeLock_ABI,
 	StakingRewards_ABI,
 	StakingRewardsFactory_ABI,
 	UniswapV2Router_ABI,
@@ -83,6 +91,7 @@ export {
 export const interfaceLookup = {
 	AdapterRegistry: AdapterRegistryInterface,
 	Erc20Adapter: Erc20AdapterInterface,
+	ERC20DividendsOwned: ERC20DividendsOwnedInterface,
 	IERC20: IERC20Interface,
 	IIndexedUniswapV2Oracle: IIndexedUniswapV2OracleInterface,
 	IndexedNarwhalRouter: IndexedNarwhalRouterInterface,
@@ -96,6 +105,7 @@ export const interfaceLookup = {
 	NirnVault: NirnVaultInterface,
 	Pair: PairInterface,
 	RewardsSchedule: RewardsScheduleInterface,
+	SharesTimeLock: SharesTimeLockInterface,
 	StakingRewards: StakingRewardsInterface,
 	StakingRewardsFactory: StakingRewardsFactoryInterface,
 	UniswapV2Router: UniswapV2RouterInterface,
