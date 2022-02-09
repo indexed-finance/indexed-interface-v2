4f719158dd3b06ed243133e186253532c22a489e
d1ll0n <dillonkellar@protonmail.com>
2022-02-08 23:27:50 -0600

    Use address hooks to select by chainId

--- src/hooks/contract-hooks.ts
+++ src/hooks/contract-hooks.ts
@@ -1,24 +1,21 @@
 import {
-  ADAPTER_REGISTRY_ADDRESS,
-  BURN_ROUTER_ADDRESS,
-  DNDX_ADDRESS,
-  DNDX_TIMELOCK_ADDRESS,
-  MASTER_CHEF_ADDRESS,
-  MINT_ROUTER_ADDRESS,
-  MULTICALL2_ADDRESS,
-  MULTI_TOKEN_STAKING_ADDRESS,
-  NARWHAL_ROUTER_ADDRESS,
-  UNISWAP_ROUTER_ADDRESS,
+	ADAPTER_REGISTRY_ADDRESS,
+	DNDX_ADDRESS,
+	DNDX_TIMELOCK_ADDRESS,
+	MASTER_CHEF_ADDRESS,
+	MULTICALL2_ADDRESS,
+	MULTI_TOKEN_STAKING_ADDRESS,
+	NARWHAL_ROUTER_ADDRESS,
+	UNISWAP_ROUTER_ADDRESS,
 } from "config";
 
 import { ContractTypeLookup, InterfaceKind, getContract } from "ethereum";
 
+import { useChainId } from "hooks";
+
 import { useSigner } from "features";
 
-export function useContractWithSigner<T extends InterfaceKind>(
-  address: string,
-  name: T
-): ContractTypeLookup[T] | undefined {
+export function useContractWithSigner<T extends InterfaceKind>(address: string | undefined, name: T): ContractTypeLookup[T] | undefined {
   const signer = useSigner();
   if (signer && address) {
     return getContract(address, name, signer);
@@ -26,83 +23,96 @@ export function useContractWithSigner<T extends InterfaceKind>(
 }
 
 export function useAdapterRegistryContract() {
-  return useContractWithSigner(ADAPTER_REGISTRY_ADDRESS, "AdapterRegistry");
+	const chainId = useChainId();
+	const address = ADAPTER_REGISTRY_ADDRESS[chainId];
+	return useContractWithSigner(address, "AdapterRegistry");
 }
 
 export function useErc20AdapterContract(address: string) {
-  return useContractWithSigner(address, "Erc20Adapter");
+	return useContractWithSigner(address, "Erc20Adapter");
 }
 
 export function useDNDXContract() {
-  return useContractWithSigner(DNDX_ADDRESS, "ERC20DividendsOwned");
-}
-
-export function useTimelockContract() {
-  return useContractWithSigner(
-    DNDX_TIMELOCK_ADDRESS.toLowerCase(),
-    "SharesTimeLock"
-  );
+	const chainId = useChainId();
+	const address = DNDX_ADDRESS[chainId];
+	return useContractWithSigner(address, "ERC20DividendsOwned");
 }
 
 export function useTokenContract(address: string) {
-  return useContractWithSigner(address, "IERC20");
+	return useContractWithSigner(address, "IERC20");
 }
 
 export function useIIndexedUniswapV2OracleContract(address: string) {
-  return useContractWithSigner(address, "IIndexedUniswapV2Oracle");
+	return useContractWithSigner(address, "IIndexedUniswapV2Oracle");
 }
 
 export function useIndexedNarwhalRouterContract() {
-  return useContractWithSigner(NARWHAL_ROUTER_ADDRESS, "IndexedNarwhalRouter");
+	const chainId = useChainId();
+	const address = NARWHAL_ROUTER_ADDRESS[chainId];
+	return useContractWithSigner(address, "IndexedNarwhalRouter");
+}
+
+export function useIndexedUniswapRouterBurnerContract(address: string) {
+	return useContractWithSigner(address, "IndexedUniswapRouterBurner");
+}
+
+export function useIndexedUniswapRouterMinterContract(address: string) {
+	return useContractWithSigner(address, "IndexedUniswapRouterMinter");
 }
 
 export function useIndexPoolContract(address: string) {
-  return useContractWithSigner(address, "IPool");
+	return useContractWithSigner(address, "IPool");
 }
 
 export function useIPoolInitializerContract(address: string) {
-  return useContractWithSigner(address, "IPoolInitializer");
+	return useContractWithSigner(address, "IPoolInitializer");
 }
 
 export function useMasterChefContract() {
-  return useContractWithSigner(MASTER_CHEF_ADDRESS, "MasterChef");
+	const chainId = useChainId();
+	const address = MASTER_CHEF_ADDRESS[chainId];
+	return useContractWithSigner(address, "MasterChef");
 }
 
 export function useMultiCall2Contract() {
-  return useContractWithSigner(MULTICALL2_ADDRESS, "MultiCall2");
+	const address = MULTICALL2_ADDRESS;
+	return useContractWithSigner(address, "MultiCall2");
 }
 
 export function useMultiTokenStakingContract() {
-  return useContractWithSigner(
-    MULTI_TOKEN_STAKING_ADDRESS,
-    "MultiTokenStaking"
-  );
+	const chainId = useChainId();
+	const address = MULTI_TOKEN_STAKING_ADDRESS[chainId];
+	return useContractWithSigner(address, "MultiTokenStaking");
 }
 
 export function useNirnVaultContract(address: string) {
-  return useContractWithSigner(address, "NirnVault");
+	return useContractWithSigner(address, "NirnVault");
 }
 
 export function usePairContract(address: string) {
-  return useContractWithSigner(address, "Pair");
+	return useContractWithSigner(address, "Pair");
 }
 
 export function useRewardsScheduleContract(address: string) {
-  return useContractWithSigner(address, "RewardsSchedule");
+	return useContractWithSigner(address, "RewardsSchedule");
 }
 
-export function useSharesTimeLockContract() {
-  return useContractWithSigner(DNDX_TIMELOCK_ADDRESS, "SharesTimeLock");
+export function useTimelockContract() {
+	const chainId = useChainId();
+	const address = DNDX_TIMELOCK_ADDRESS[chainId];
+	return useContractWithSigner(address, "SharesTimeLock");
 }
 
 export function useStakingRewardsContract(address: string) {
-  return useContractWithSigner(address, "StakingRewards");
+	return useContractWithSigner(address, "StakingRewards");
 }
 
 export function useStakingRewardsFactoryContract(address: string) {
-  return useContractWithSigner(address, "StakingRewardsFactory");
+	return useContractWithSigner(address, "StakingRewardsFactory");
 }
 
 export function useUniswapRouterContract() {
-  return useContractWithSigner(UNISWAP_ROUTER_ADDRESS, "UniswapV2Router");
+	const chainId = useChainId();
+	const address = UNISWAP_ROUTER_ADDRESS[chainId];
+	return useContractWithSigner(address, "UniswapV2Router");
 }
