6a923820009cf4b359efd36e38bfa8e9ddcb9658
d1ll0n <dillonkellar@protonmail.com>
2022-02-09 00:20:49 -0600

    Use chainId to build external links to explorer/exchange

--- src/helpers/links.ts
+++ src/helpers/links.ts
@@ -1,28 +1,25 @@
 import { ChainId } from "@indexed-finance/narwhal-sdk";
+import { NETWORKS_BY_ID } from "config";
 
-const EXPLORER: Record<number, string> = {
-  [ChainId.MAINNET]: 'https://etherscan.io',
-  [ChainId.POLYGON]: 'https://polygonscan.com'
-};
-
-const UNISWAP_INFO: Record<number, string> = {
-  [ChainId.MAINNET]: 'https://v2.info.uniswap.org',
-  [ChainId.POLYGON]: 'https://info.quickswap.exchange/#'
-};
-
-const SUSHISWAP_INFO: Record<number, string> = {
+const SUSHISWAP_INFO_URL: Record<number, string> = {
   [ChainId.MAINNET]: 'https://analytics.sushi.com',
   [ChainId.POLYGON]: 'https://analytics-polygon.sushi.com'
 }
 
-export const etherscanAddressLink = (address: string, network = 1) => `${EXPLORER[network]}/address/${address}`;
-export const etherscanTokenLink = (address: string, network = 1) => `${EXPLORER[network]}/token/${address}`;
-export const etherscanTransactionLink = (tx: string, network = 1) => `${EXPLORER[network]}/tx/${tx}`;
+export const explorerAddressLink = (address: string, chainId = 1) => `${NETWORKS_BY_ID[chainId].explorer.url}/address/${address}`;
+export const explorerTokenLink = (address: string, chainId = 1) => `${NETWORKS_BY_ID[chainId].explorer.url}/token/${address}`;
+export const explorerTransactionLink = (tx: string, chainId = 1) => `${NETWORKS_BY_ID[chainId].explorer.url}/tx/${tx}`;
 
-export const uniswapInfoTokenLink = (address: string, network = 1) => `${UNISWAP_INFO[network]}/token/${address}`;
-export const uniswapInfoPairLink = (address: string, network = 1) => `${UNISWAP_INFO[network]}/pair/${address}`;
+export const uniswapInfoTokenLink = (address: string, chainId = 1) => `${NETWORKS_BY_ID[chainId].defaultExchange.infoUrl}/token/${address}`;
+export const uniswapInfoPairLink = (address: string, chainId = 1) => `${NETWORKS_BY_ID[chainId].defaultExchange.infoUrl}/pair/${address}`;
 
-export const sushiswapInfoTokenLink = (address: string, network = 1) => `${SUSHISWAP_INFO[network]}.sushi.com/tokens/${address}`;
-export const sushiswapInfoPairLink = (address: string, network = 1) => `${SUSHISWAP_INFO[network]}.sushi.com/pairs/${address}`;
+export const exchangeAddLiquidityLink = (token1: string, token2 = "ETH", chainId: number) =>
+  `${NETWORKS_BY_ID[chainId].defaultExchange.addLiquidityUrl}${token1}/${token2}`;
 
-export const sushiswapAddLiquidityLink = (token0: string, token1: string, network = 1) => `https://app.sushi.com/add/${token0}/${token1}`
+export const exchangeSwapLink = (buyToken: string, sellToken = "ETH", chainId: number) =>
+  `${NETWORKS_BY_ID[chainId].defaultExchange.swapUrl}inputCurrency=${sellToken}&outputCurrency=${buyToken}`
+
+export const sushiswapInfoTokenLink = (address: string, chainId = 1) => `${SUSHISWAP_INFO_URL[chainId]}.sushi.com/tokens/${address}`;
+export const sushiswapInfoPairLink = (address: string, chainId = 1) => `${SUSHISWAP_INFO_URL[chainId]}.sushi.com/pairs/${address}`;
+
+export const sushiswapAddLiquidityLink = (token0: string, token1: string, chainId = 1) => `https://app.sushi.com/add/${token0}/${token1}`
