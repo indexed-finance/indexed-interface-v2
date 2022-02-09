3c49f383f3e9a19f0cde15863070150e2b41a34a
d1ll0n <dillonkellar@protonmail.com>
2022-02-09 03:18:13 -0600

    cache ids and allow for null pool address

--- src/hooks/pool-hooks.ts
+++ src/hooks/pool-hooks.ts
@@ -1,14 +1,13 @@
 import { AppState, FormattedIndexPool, POOL_CALLER, selectors } from "features";
+import { useCachedValue } from "./use-debounce";
 import { useCallRegistrar } from "./use-call-registrar";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import type { RegisteredCall } from "helpers";
 
-export const useAllPoolIds = () =>
-  useSelector((state: AppState) => selectors.selectAllPoolIds(state));
+export const useAllPoolIds = () => useSelector(selectors.selectAllPoolIds);
 
-export const useAllPools = () =>
-  useSelector((state: AppState) => selectors.selectAllPools(state));
+export const useAllPools = () => useSelector(selectors.selectAllPools);
 
 export const usePool = (poolId: string) =>
   useSelector((state: AppState) => selectors.selectPool(state, poolId));
@@ -35,14 +34,16 @@ export const usePoolToTokens = (pool: FormattedIndexPool) => {
   );
 };
 
-export const usePoolQuickswapLink = (poolAddress: string) => {
+export const useFormattedIndexPool = (pool?: string) => useSelector((state: AppState) => pool ? selectors.selectFormattedIndexPool(state, pool) : null)
+
+export const getPoolQuickswapLink = (poolAddress: string) => {
   const poolAddressToQuickswapToken: Record<string, string> = {
     // CC10
     "0x17ac188e09a7890a1844e5e65471fe8b0ccfadf3":
-      "0x9c49ba0212bb5db371e66b59d1565b7c06e4894e",
+    "0x9c49ba0212bb5db371e66b59d1565b7c06e4894e",
     // DEFI5
     "0xfa6de2697d59e88ed7fc4dfe5a33dac43565ea41":
-      "0x42435f467d33e5c4146a4e8893976ef12bbce762",
+    "0x42435f467d33e5c4146a4e8893976ef12bbce762",
     // DEGEN
     "0x126c121f99e1e211df2e5f8de2d96fa36647c855":
       "0x8a2870fb69a90000d6439b7adfb01d4ba383a415",
@@ -132,17 +133,21 @@ export function usePoolDetailRegistrar(
   poolAddress: string,
   tokenIds: string[]
 ) {
+  const cachedIds = useCachedValue(tokenIds);
   const caller = POOL_CALLER;
   const { onChainCalls, offChainCalls } = useMemo(
     () =>
       poolAddress
-        ? createPoolDetailCalls(poolAddress, tokenIds)
+        ? createPoolDetailCalls(poolAddress, cachedIds)
         : {
             onChainCalls: [],
             offChainCalls: [],
           },
-    [poolAddress, tokenIds]
+    [poolAddress, cachedIds]
   );
+  useEffect(() => {
+    console.log(`Pool Detail Registrar: calls changed`);
+  }, [onChainCalls, offChainCalls])
 
   useCallRegistrar({
     caller,
